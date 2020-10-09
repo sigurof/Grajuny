6f3dbf67986266396ff75378f87b12b0c4af84f8
Sigurd Ofstad <sigurof@gmail.com>
2020-10-09 22:22:23 +0200

    develop - merged in feature/scene-graph to develop and resolved conflicts

--- src/main/kotlin/no/sigurof/grajuny/utils/Constants.kt
+++ src/main/kotlin/no/sigurof/grajuny/utils/Constants.kt
@@ -4,10 +4,3 @@ import org.joml.Vector3f
 
 val ORIGIN = Vector3f(0f, 0f, 0f)
 
-val WHITE = Vector3f(1f, 1f, 1f)
-val RED = Vector3f(1f, 0f, 0f)
-val GREEN = Vector3f(0f, 1f, 0f)
-val BLUE = Vector3f(0f, 0f, 1f)
-val PINK = Vector3f(1f, 0f, 1f)
-val YELLOW = Vector3f(1f, 1f, 0f)
-val CYAN = Vector3f(0f, 1f, 1f)
